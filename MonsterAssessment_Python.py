cf40ab73b1b1e13c320e3c74f031338266947949
LucaTaylor1 <lucataylor@cromwell.school.nz>
2025-08-15 10:50:01 +1200

    Finished: Added the ability to make your own monster and fixed some stuff

--- MonsterAssessment_Python.py
+++ MonsterAssessment_Python.py
@@ -57,7 +57,7 @@ easygui.msgbox("Welcome to the Monster Card Index!")
 while Restart == 1:
     menu = easygui.buttonbox("What would you like to do today?", title=["Monster Card Index"], choices=["View Cards", "Edit/Delete Cards", "Add New Card", "Exit"])
     if menu == "Exit":
-        easygui.msgbox("Very Well.")
+        easygui.msgbox("Very Well. Goodbye.")
         sys.exit()
         
     elif menu == "View Cards":
@@ -81,11 +81,15 @@ while Restart == 1:
             if chosenmonster == None:
                 break
             id_value = Monsters[chosenmonster]
-            choice2 = easygui.buttonbox("Would you like to edit or delete " + chosenmonster + "?", choices=["Edit", "Delete"])
+            choice2 = easygui.buttonbox("Would you like to edit or delete " + chosenmonster + "?", choices=["Edit", "Delete", "Cancel"])
             if choice2 == "Delete":
-                removed_monster = Monsters.pop(chosenmonster)
-                easygui.msgbox("The card " + chosenmonster + " has been deleted.")
-                break
+                confirmation = easygui.buttonbox("Are you certain you want to delete " + chosenmonster + "?", choices=["Yes", "No"])
+                if confirmation == "No":
+                    break
+                else:
+                    removed_monster = Monsters.pop(chosenmonster)
+                    easygui.msgbox("The card " + chosenmonster + " has been deleted.")
+                    break
             if choice2 == "Edit":
                 id_value = Monsters[chosenmonster]
                 message = f"{chosenmonster}\n" + "-" * len(chosenmonster) + "\n"
@@ -100,21 +104,60 @@ while Restart == 1:
                     break
                 Monsters[chosenmonster][editchoice] = Newvalue
                 easygui.msgbox(f"{editchoice} has been updated to {Newvalue} for {chosenmonster}.")
+            if choice2 == "Cancel":
+                break
 
     elif menu == "Add New Card":
         while True:
-                 
-    
+            card_name = easygui.enterbox("What would you like to name your new card?")
 
+            if card_name == None:
+                break
+            elif card_name.strip() == "":
+                easygui.msgbox("Name cannot be blank")
+            elif card_name in Monsters:
+                easygui.msgbox("The monster " + card_name + " already exists!")
+                continue
+            else:
+                msg = "What stats would you like for " + card_name + "?"
+                title = card_name + "s' Stats"
+                Stats = ["Strength", "Speed", "Stealth", "Cunning"]
+                statstest = easygui.multenterbox(msg, title, Stats)
+                if statstest == None:
+                    break
 
+                strength = statstest[0] if len(statstest) > 0 else ""
+                speed = statstest[1] if len(statstest) > 1 else ""
+                stealth = statstest[2] if len(statstest) > 2 else ""
+                cunning = statstest[3] if len(statstest) > 3 else ""
 
+                if strength == "" or speed == "" or stealth == "" or cunning == "":
+                    easygui.msgbox("All four stat boxes must be filled in.")
+                    continue
 
+                str = int(strength)
+                spe = int(speed)
+                ste = int(stealth)
+                cun = int(cunning)
 
+                if str <= 0 or str >= 26:
+                    easygui.msgbox("Keep Strength between 1 and 25!")
+                    continue
 
+                if spe <= 0 or spe >= 26:
+                    easygui.msgbox("Keep Speed between 1 and 25!")
+                    continue
 
+                if ste <= 0 or ste >= 26:
+                    easygui.msgbox("Keep Stealth between 1 and 25!")
+                    continue
 
+                if cun <= 0 or cun >= 26:
+                    easygui.msgbox("Keep Cunning between 1 and 25!")
+                    continue
 
-
-
-
-
+                Monsters[card_name] = {"Strength" : statstest[0], "Speed" : statstest[1],
+                                       "Stealth" : statstest[2], "Cunning" : statstest[3]}
+                
+                easygui.msgbox("The card " + card_name + " has been successfully added!")
+                break
